f31b3bb4ce63b628a0bdf897f52ac101fb59e808
Shashwat Mishra <shashwatmm1996@gmail.com>
2019-04-04 19:41:17 +0530

    seocnd

--- testfrominstance/app.py
+++ testfrominstance/app.py
@@ -1,175 +1,46 @@
-import json
-import os
-import random
-
-import requests
-from chalice import Chalice, ChaliceViewError, CORSConfig, Rate
-
-from chalicelib.aws_secrets_helper import get_secret
-from chalicelib.services.mail import send_mail
-from chalicelib.services.notify_meetings import notify_helper
-from chalicelib.services.reminder import *
-from chalicelib.services.sms import send_sms
-from chalicelib.services.smsTemplates import Templates
+from chalice import Chalice
 from chalicelib.util import get_session
+from chalice import CORSConfig
 
-app = Chalice(app_name = 'communication')
-app.debug = True
+from chalicelib.db import db, base
 
-
-communication_host = json.loads(get_secret())["communicationHost"]
+app = Chalice(app_name='testfrominstance')
+base.metadata.create_all(db)
 
 cors_config = CORSConfig(
-    allow_origin = '*',
-    allow_headers = ['X-Special-Header'],
-    max_age = 600,
-    expose_headers = ['X-Special-Header']
+allow_origin='*',
+allow_headers=['X-Special-Header'],
+max_age=600,
+expose_headers=['X-Special-Header'],
+allow_credentials=True
 )
 
-def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-    """
-    auth = app.current_request.headers.get("Authorization", None)
-    parts = auth.split()
+@app.route('/')
+def index():	
+	return {'hello': 'world'}
 
-    token = parts[1]
-    return token
+@app.route('/print', cors=cors_config)
+def get_earned_by_id():
+	with get_session() as session:
+		x = session.execute("select * from testing where name='miku';").fetchone()[0]
+		return str(x)
 
-from chalicelib.auth_helper import (requires_auth, requires_auth_mentee,requires_auth_mentor)
-
-
-# structure of credentials should be
-# credentials = [
-#                   {
-#                        number: ''(empty)/'+11234567891'(number for the sms support),
-#                        email_id: ''(empty)/'xyz@harvard.edu' (email id for the mail support),
-#                        data: 'data for the template',
-#                        template_id: "id of the tempate",
-#                        from: "sender email id",
-#                        message: "message to be sent via sms"
-#                   }
-#               ]
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
 #
-# * All fields are mandatory
-
-@app.route('/notifyEvents', methods = ['GET', 'POST'], cors = cors_config)
-def notifyEvents1():
-    request = app.current_request
-    if request.method == 'POST':
-        sms = request.json_body['isSMS']        # This is a boolean field. True if notified via SMS
-        mail = request.json_body['isMail']      # This is a boolean field. True if notified via e-mail
-        body = request.json_body['credentials']
-        send_mail(body)
-
-        if(sms):
-            send_sms(body)
-
-        return("Sent successfully")
-            
-    return("Welcome")
-
-@app.route('/notifyMentee', methods= ['POST'], cors = cors_config)
-def notifyMentee():
-    request = app.current_request
-    if request.method == 'POST':
-        menteeId = request.json_body['menteeId']
-        template_id = request.json_body['template_id']
-        data = request.json_body['data']
-        sms_data = request.json_body['sms_data']
-
-        notify_helper(menteeId, template_id, data, "mentee", sms_data)
-
-        return ("Mentee Notified")
-        
-    return {'hello': 'world'}
-
-@app.route('/notifyMentor', methods= ['POST'], cors = cors_config)
-def notifyMentor():
-    request = app.current_request
-    if request.method == 'POST':
-        mentorId = request.json_body['mentorId']
-        template_id = request.json_body['template_id']
-        data = request.json_body['data']
-        sms_data = request.json_body['sms_data']
-
-        notify_helper(mentorId, template_id, data, "mentor", sms_data)
-
-        return("Mentor Notified")
-        
-    return {'notify Mentor'}
-
-@app.route('/contact', methods = ['POST'], cors = cors_config)
-def contact():
-    request = app.current_request
-    if request.method == 'POST':
-        firstName = request.json_body["firstName"]
-        email = request.json_body["email"]
-        phoneNumber = request.json_body["phoneNumber"]
-        message = request.json_body["message"]
-
-        email_data = {
-            "isSMS": False,
-            "isMail": True,
-            "credentials": {
-                "number": " ",
-                "email_id": json.loads(get_secret())["admin_email_id"],
-                "cc": email,
-                "data": {
-                    "sender_first_name": firstName,
-                    "sender_message": message
-                },
-                "template_id": "ADMIN_CONTACT",
-                "from": json.loads(get_secret())["admin_email_id"],
-                "message": " "
-            }
-        }
-
-        requests.post(communication_host + "notifyEvents", json=email_data)
-
-        print("data sent to admin successfully")
-
-@app.route('/sendSMS', methods = ['POST'], cors = cors_config)
-def run_send_sms():
-    request = app.current_request
-    if request.method == 'POST':
-        to = request.json_body['to']
-        template_id = request.json_body['template_id']
-        sms_data = request.json_body['sms_data']
-        print("sms_data: " + str(sms_data))
-        message = Templates(template_id, sms_data)
-
-        body = {
-            "number": to,
-            "message": message
-        }
-
-        return send_sms(body)
-
-@app.route('/reminders',methods = ['POST'], cors = cors_config)
-def new_reminder():
-    try:
-        request = app.current_request
-        if request.method == 'POST':
-            add_reminder(request.json_body)
-	
-    except Exception as e:
-        print(e)
-        raise ChaliceViewError(e)
-
-@app.schedule("cron(0/15 * * * ? *)")
-def run_reminder(event):
-    try:
-        execute_reminders()
-	
-    except Exception as e:
-        print(e)
-        raise ChaliceViewError(e)
-
-@app.route('/declineReminders/{appointment_id}',methods = ['POST'], cors = cors_config)
-def decline_reminders(appointment_id):
-    try:
-        decline_reminder(appointment_id)
-
-    except Exception as e:
-        print(e)
-        raise ChaliceViewError(e)
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
--- testfrominstance/chalicelib/db.py
+++ testfrominstance/chalicelib/db.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import os
-from .aws_secrets_helper import get_secret
 import json
 
-db = create_engine(json.loads(get_secret())["DB_URI"])
+db = create_engine("postgresql+psycopg2://zemoso:Miku1506@54.236.34.225:5432/zemoso")
 db.echo = True
 
 base = declarative_base()
